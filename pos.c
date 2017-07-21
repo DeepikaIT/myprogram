26813f2f56d79d8562c8598688b3879586e88cc8
DeepikaIT <30291360+DeepikaIT@users.noreply.github.com>
2017-07-21 17:08:02 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main()
+ {
+double no;
+ printf("enter a no");
+ scanf("if",&no);
+ if(no<0.0)
+ printf("enter  negative no");
+ if(no>0.0)
+ printf("ender  positive no");
+ else 
+ printf("enter o");
+ return 0;
+ }
